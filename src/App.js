5c7787ae2a37a2434d4ae9896f3255ca2c64f302
abdulrafayghani <abdulrafay.ghani@gmail.com>
2020-07-21 17:49:33 +0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import useWebAnimations from "@wellyshen/use-web-animations";
 
 
@@ -25,27 +25,27 @@ function App() {
     { transform: 'translateY(-100%)' }   
   ];
 
-  const { ref: b1 } = useWebAnimations({
+  const { ref: b1, getAnimation: ba1 } = useWebAnimations({
     keyframes: sceneryFrames,
     timing: sceneryTimingBackground
   })
 
-  const { ref: b2 } = useWebAnimations({
+  const { ref: b2, getAnimation: ba2 } = useWebAnimations({
     keyframes: sceneryFrames,
     timing: sceneryTimingBackground
   })
 
-  const { ref: f1 } = useWebAnimations({
+  const { ref: f1, getAnimation: fa1 } = useWebAnimations({
     keyframes: sceneryFrames,
     timing: sceneryTimingForeground
   })
 
-  const { ref: f2 } = useWebAnimations({
+  const { ref: f2, getAnimation: fa2 } = useWebAnimations({
     keyframes: sceneryFrames,
     timing: sceneryTimingForeground
   })
 
-  const { ref: rq } = useWebAnimations({
+  const { ref: rq, getAnimation: rqa } = useWebAnimations({
     keyframes: spriteFrames,
     timing:{
       easing: 'steps(7, end)',
@@ -53,37 +53,20 @@ function App() {
       duration: 600,
       iterations: Infinity
     },
-    playbackRate: 1
   })
 
-  const sceneries = [f1, f2, b1, b2]
-
-  const adjustBackgroundPlayback = () =>{
-    if(rq.playbackRate < .8){
-      sceneries.forEach(( anim )=>{
-        anim.playbackRate = rq.playbackRate/2 * -1
-      })
-    }else if(rq.playbackRate > 1.2){
-      sceneries.forEach(( anim )=>{
-        anim.playbackRate = 0
-      })
-    }
-  }
-  adjustBackgroundPlayback()
-
-  setInterval( function() {
-    /* Set decay */
-    if (rq.playbackRate > .4) {
-        rq.playbackRate *= .9;    
-    } 
-    adjustBackgroundPlayback();
-  }, 3000);
-
   const goFaster = () =>{
-    rq.playbackRate += 1.1
-    adjustBackgroundPlayback()
+    const animation1 = ba1()
+    const animation2 = ba2()
+    const animation3 = fa1()
+    const animation4 = fa2()
+    const animation5 = rqa()
+    animation1.updatePlaybackRate(animation1.playbackRate * 1.1)
+    animation2.updatePlaybackRate(animation2.playbackRate * 1.1)
+    animation3.updatePlaybackRate(animation3.playbackRate * 1.1)
+    animation4.updatePlaybackRate(animation4.playbackRate * 1.1)
+    animation5.updatePlaybackRate(animation5.playbackRate * 1.1)
   }
-  window.addEventListener('click',goFaster)
 
   return (
       <div className="wrapper">
@@ -93,9 +76,9 @@ function App() {
             <img
               id="red-queen_and_alice_sprite"
               src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
-              // srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x"
               alt="Alice and the Red Queen running to stay in place."
               ref={rq}
+              onClick={goFaster}
             />
           </div>
         </div>
@@ -104,7 +87,6 @@ function App() {
           <img
             id="palm3"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
-            // srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"
             alt=" "
           />
         </div>
@@ -112,13 +94,11 @@ function App() {
           <img
             id="bush"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png"
-            // srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x"
             alt=" "
           />
           <img
             id="w_rook_upright"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png"
-            // srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x"
             alt=" "
           />
         </div>
@@ -126,19 +106,16 @@ function App() {
           <img
             id="r_pawn_upright"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
-            // srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x"
             alt=" "
           />
           <img
             id="w_rook"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png"
-            // srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x"
             alt=" "
           />
           <img
             id="palm1"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"
-            // srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x"
             alt=" "
           />
         </div>
@@ -146,20 +123,17 @@ function App() {
           <img
             id="r_pawn"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png"
-            // srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x"
             alt=" "
           />
 
           <img
             id="r_knight"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png"
-            // srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x"
             alt=" "
           />
           <img
             id="palm2"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png"
-            // srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x"
             alt=" "
           />
         </div>
